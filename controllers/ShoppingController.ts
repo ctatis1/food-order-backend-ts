e5092b6ce53508b12b2cede18423246510cef7b8
Camilo <52603058+ctatis1@users.noreply.github.com>
2022-07-13 12:26:16 -0500

    GET restaurant by id and Search foods by pincode

--- controllers/ShoppingController.ts
+++ controllers/ShoppingController.ts
@@ -31,7 +31,7 @@ export const GetFoodIn30Min =async (req: Request, res: Response, next: NextFunct
 
         result.map(restaurant => {
             const foods = restaurant.foods as [FoodDoc]
-            foodResult.push(...foods.filter(food => food.readyTime <= 30))
+            foodResult.push(...foods.filter(food => food.readyTime < 30))
         })
 
         return res.status(200).json(foodResult)
@@ -43,9 +43,33 @@ export const GetFoodIn30Min =async (req: Request, res: Response, next: NextFunct
 
 export const SearchFoods =async (req: Request, res: Response, next: NextFunction) => {
 
+    const pincode = req.params.pincode;
+
+    const result = await Restaurant.find({ pincode: pincode, serviceAvailable: true }).populate('foods')
+
+    if(result.length > 0){
+        let foodResult: any = [];
+
+        result.map(item => foodResult.push(...item.foods))
+
+        return res.status(200).json(foodResult)
+    }
+
+    return res.status(400).json({ message: 'No Restaurants Found' })
+
 }
 
 export const RestaurantById =async (req: Request, res: Response, next: NextFunction) => {
+
+    const id = req.params.id
+
+    const result = await Restaurant.findById(id).populate('foods')
+
+    if(result){
+        return res.json(result)
+    }
+
+    return res.status(400).json({ message: 'No Restaurants Found' })
 
 }
 
