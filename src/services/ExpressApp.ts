9935eb26c820a9d0f535e1972c2317ff9cd2062b
Camilo <52603058+ctatis1@users.noreply.github.com>
2022-07-25 10:23:51 -0500

    reformat project structure

--- src/services/ExpressApp.ts
+++ src/services/ExpressApp.ts
@@ -1,11 +1,10 @@
 import express, { Application } from 'express';
-import bodyParser from 'body-parser';
 import {AdminRoute, RestaurantRoute, ShoppingRoute, CustomerRoute} from '../routes'
 import path from 'path'
 
 export default async( app: Application) => {
-    app.use(bodyParser.json())
-    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true }))
     app.use('/images', express.static(path.join(__dirname, 'images')))
 
     app.use('/admin', AdminRoute);
